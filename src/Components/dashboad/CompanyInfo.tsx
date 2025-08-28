fe1b44f4e0aab23d89c9a9400a6f07268a0abf55
mugilane.cse <55045435+Mugilane@users.noreply.github.com>
2025-08-28 17:54:09 +0530

    dash

--- src/Components/dashboad/CompanyInfo.tsx
+++ src/Components/dashboad/CompanyInfo.tsx
@@ -2,42 +2,78 @@
 import React from 'react'
 import yoho from '../../assets/Navbar/yohologo.png'
 import { FONTS } from '../../constants/uiconstants'
+import { MobileResponsive } from '../../hooks/MobileResponsive'
 
 interface props {
     company_details?: any
 }
 
 const CompanyInfo: React.FC<props> = ({ company_details }) => {
+
+    const MobileView = MobileResponsive()
+
     return (
         <div className='w-full p-4 flex flex-col items-center gap-5' >
-            <div className="flex flex-row w-full gap-10">
-                <div className='w-80 p-2 flex items-center'>
-                    <img src={yoho} alt="" className=' object-cover w-full' />
-                </div>
-                <div className="grid grid-cols-2 gap-5 w-full">
-                    <div className="w-full bg-[#EAEBE8] shadow-[0px_0px_15px_0px_#4A70791A] p-4 flex flex-col gap-5 rounded-xl">
-                        <h1 style={{ ...FONTS.table_head }} className='text-[#4A7079] !font-bold'>Organsation Name:</h1>
-                        <h1 style={{ ...FONTS.table_data }} className='text-[#4A7079] !font-medium break-words'>{company_details?.company_name}</h1>
-                    </div>
-                    <div className="w-full bg-[#EAEBE8] shadow-[0px_0px_15px_0px_#4A70791A] p-4 flex flex-col gap-5 rounded-xl">
-                        <h1 style={{ ...FONTS.table_head }} className='text-[#4A7079] !font-bold'>Organsation Mail:</h1>
-                        <h1 style={{ ...FONTS.table_data }} className='text-[#4A7079] !font-medium break-words'>{company_details?.contact_info?.email}</h1>
-                    </div>
-                    <div className="w-full bg-[#EAEBE8] shadow-[0px_0px_15px_0px_#4A70791A] p-4 flex flex-col gap-5 rounded-xl">
-                        <h1 style={{ ...FONTS.table_head }} className='text-[#4A7079] !font-bold'>Organsation Phone:</h1>
-                        <h1 style={{ ...FONTS.table_data }} className='text-[#4A7079] !font-medium break-words'>{company_details?.contact_info?.phone}</h1>
-                    </div>
-                    <div className="w-full bg-[#EAEBE8] shadow-[0px_0px_15px_0px_#4A70791A] p-4 flex flex-col gap-5 rounded-xl">
-                        <h1 style={{ ...FONTS.table_head }} className='text-[#4A7079] !font-bold'>website:</h1>
-                        <h1 style={{ ...FONTS.table_data }} className='text-[#4A7079] !font-medium break-words'>{company_details?.website}</h1>
-                    </div>
-                </div>
+            {
+                MobileView ?
+                    (<>
+                        <div className="flex flex-col w-full gap-10">
+                            <div className="grid grid-cols-2 gap-5 w-full">
+                                <div className='w-32 p-2 flex items-center'>
+                                    <img src={yoho} alt="" className=' object-cover w-full' />
+                                </div>
+                                <div className="w-full bg-[#EAEBE8] shadow-[0px_0px_15px_0px_#4A70791A] p-4 flex flex-col gap-5 rounded-xl">
+                                    <h1 style={{ ...FONTS.table_head }} className='text-[#4A7079] !text-[12px] !font-bold'>Organsation Name:</h1>
+                                    <h1 style={{ ...FONTS.table_data }} className='text-[#4A7079] !text-[10px] !font-medium break-words'>{company_details?.company_name}</h1>
+                                </div>
+                                <div className="w-full bg-[#EAEBE8] shadow-[0px_0px_15px_0px_#4A70791A] p-4 flex flex-col gap-5 rounded-xl">
+                                    <h1 style={{ ...FONTS.table_head }} className='text-[#4A7079] !text-[12px] !font-bold'>Organsation Mail:</h1>
+                                    <h1 style={{ ...FONTS.table_data }} className='text-[#4A7079] !text-[10px] !font-medium break-words'>{company_details?.contact_info?.email}</h1>
+                                </div>
+                                <div className="w-full bg-[#EAEBE8] shadow-[0px_0px_15px_0px_#4A70791A] p-4 flex flex-col gap-5 rounded-xl">
+                                    <h1 style={{ ...FONTS.table_head }} className='text-[#4A7079] !text-[12px] !font-bold'>Organsation Phone:</h1>
+                                    <h1 style={{ ...FONTS.table_data }} className='text-[#4A7079] !text-[10px] !font-medium break-words'>{company_details?.contact_info?.phone}</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-full bg-[#EAEBE8] shadow-[0px_0px_15px_0px_#4A70791A] p-4 flex flex-col gap-5 rounded-xl">
+                            <h1 style={{ ...FONTS.table_head }} className='text-[#4A7079] !text-[12px] !font-bold'>Organsation Address:</h1>
+                            <h1 style={{ ...FONTS.table_data }} className='text-[#4A7079] !text-[10px] !font-medium break-words'>{company_details?.contact_info?.address}</h1>
+                        </div>
+                    </>)
+                    :
+                    (<>
+                        <div className="flex flex-row w-full gap-10">
+                            <div className='w-80 p-2 flex items-center'>
+                                <img src={yoho} alt="" className=' object-cover w-full' />
+                            </div>
+                            <div className="grid grid-cols-2 gap-5 w-full">
+                                <div className="w-full bg-[#EAEBE8] shadow-[0px_0px_15px_0px_#4A70791A] p-4 flex flex-col gap-5 rounded-xl">
+                                    <h1 style={{ ...FONTS.table_head }} className='text-[#4A7079] !font-bold'>Organsation Name:</h1>
+                                    <h1 style={{ ...FONTS.table_data }} className='text-[#4A7079] !font-medium break-words'>{company_details?.company_name}</h1>
+                                </div>
+                                <div className="w-full bg-[#EAEBE8] shadow-[0px_0px_15px_0px_#4A70791A] p-4 flex flex-col gap-5 rounded-xl">
+                                    <h1 style={{ ...FONTS.table_head }} className='text-[#4A7079] !font-bold'>Organsation Mail:</h1>
+                                    <h1 style={{ ...FONTS.table_data }} className='text-[#4A7079] !font-medium break-words'>{company_details?.contact_info?.email}</h1>
+                                </div>
+                                <div className="w-full bg-[#EAEBE8] shadow-[0px_0px_15px_0px_#4A70791A] p-4 flex flex-col gap-5 rounded-xl">
+                                    <h1 style={{ ...FONTS.table_head }} className='text-[#4A7079] !font-bold'>Organsation Phone:</h1>
+                                    <h1 style={{ ...FONTS.table_data }} className='text-[#4A7079] !font-medium break-words'>{company_details?.contact_info?.phone}</h1>
+                                </div>
+                                <div className="w-full bg-[#EAEBE8] shadow-[0px_0px_15px_0px_#4A70791A] p-4 flex flex-col gap-5 rounded-xl">
+                                    <h1 style={{ ...FONTS.table_head }} className='text-[#4A7079] !font-bold'>website:</h1>
+                                    <h1 style={{ ...FONTS.table_data }} className='text-[#4A7079] !font-medium break-words'>{company_details?.website}</h1>
+                                </div>
+                            </div>
 
-            </div>
-            <div className="w-full bg-[#EAEBE8] shadow-[0px_0px_15px_0px_#4A70791A] p-4 flex flex-col gap-5 rounded-xl">
-                <h1 style={{ ...FONTS.table_head }} className='text-[#4A7079] !font-bold'>Organsation Address:</h1>
-                <h1 style={{ ...FONTS.table_data }} className='text-[#4A7079] !font-medium break-words'>{company_details?.contact_info?.address}</h1>
-            </div>
+                        </div>
+                        <div className="w-full bg-[#EAEBE8] shadow-[0px_0px_15px_0px_#4A70791A] p-4 flex flex-col gap-5 rounded-xl">
+                            <h1 style={{ ...FONTS.table_head }} className='text-[#4A7079] !font-bold'>Organsation Address:</h1>
+                            <h1 style={{ ...FONTS.table_data }} className='text-[#4A7079] !font-medium break-words'>{company_details?.contact_info?.address}</h1>
+                        </div>
+                    </>)
+            }
+
         </div>
     )
 }
--- src/pages/Employer/EmployerProfile.tsx
+++ src/pages/Employer/EmployerProfile.tsx
@@ -42,7 +42,7 @@ const EmployerProfile: React.FC = () => {
     }, [dispatch, role, uuid]);
 
     return (
-        <div className='overflow-y-scroll h-full scrollbar-hide'>
+        <div className={`overflow-y-scroll h-full scrollbar-hide ${MobileView && 'overflow-x-hidden'}`}>
 
             {role === "employee" && <CompanyInfo company_details={employer?.company_id} />}
 
@@ -59,18 +59,18 @@ const EmployerProfile: React.FC = () => {
                                 <div className="bg-[#7697A0] w-max p-2 px-6 h-max rounded-lg font-bold text-white text-md" onClick={() => handleDownloadMonth(choseDate, employer?.uuid)}>Generate</div>
                             </div>
                             <div className="flex flex-col w-full h-[75vh] mx-4 rounded-3xl gap-5 py-4">
-                                <div className="w-full h-52 bg-[#EAEBE8] rounded-2xl">
+                                <div className={`w-full ${MobileView ? 'h-42 -ml-2' : 'h-52'}  bg-[#EAEBE8] rounded-2xl`}>
                                     <EmployerPrevSlip payslip={len ? employer?.payroll_slip?.[len - 1] : undefined} />
                                 </div>
 
-                                <div className="w-full h-96 bg-[#EAEBE8] rounded-2xl px-4 overflow-y-scroll scrollbar-hide">
-                                    <h1 style={{ ...FONTS.payroll_head, color: COLORS.primary }} className='pt-4'>Previous PaySlips</h1>
+                                <div className={`w-full h-96 bg-[#EAEBE8] rounded-2xl px-4 overflow-y-scroll scrollbar-hide ${MobileView && '-ml-2'}`}>
+                                    <h1 style={{ ...FONTS.payroll_head, color: COLORS.primary }} className={`${MobileView && '!text-[14px]'} pt-4`}>Previous PaySlips</h1>
 
                                     <div className="flex justify-between gap-6">
                                         <div className="w-full max-h-80 " style={{ scrollbarWidth: 'none' }}>
                                             <table className="w-full border-separate border-spacing-y-4" >
                                                 <thead className="sticky top-0">
-                                                    <tr style={{ background: COLORS.primary }} className="text-left text-white rounded-lg">
+                                                    <tr style={{ background: COLORS.primary }} className={`text-left text-white rounded-lg ${MobileView && '**:!text-[12px]'}`}>
                                                         <th style={{ ...FONTS.table_head }} className="px-4 py-3 rounded-l-lg">Date</th>
                                                         <th style={{ ...FONTS.table_head }} className="px-4 py-3">Wrk Days</th>
                                                         <th style={{ ...FONTS.table_head }} className="px-4 py-3">Credited Amt</th>
@@ -80,7 +80,7 @@ const EmployerProfile: React.FC = () => {
 
                                                 <tbody>
                                                     {employer?.payroll_slip && employer?.payroll_slip?.length > 0 ? employer?.payroll_slip?.map((items: any, index: any) => (
-                                                        <tr key={index} style={{ color: COLORS.primary }} className="bg-[#F8F8F8] rounded-lg">
+                                                        <tr key={index} style={{ color: COLORS.primary }} className={`bg-[#F8F8F8] rounded-lg ${MobileView && '**:!text-[10px]'}`}>
                                                             <td style={{ ...FONTS.table_data }} className="px-4 py-3 rounded-l-lg"> {dayjs(items?.created_month).format("MMMM-YYYY")}</td>
                                                             <td style={{ ...FONTS.table_data }} className="px-4 py-3">{items?.paid_days}</td>
                                                             <td style={{ ...FONTS.table_data }} className="px-4 py-3">{items?.gross_total}</td>
@@ -89,10 +89,10 @@ const EmployerProfile: React.FC = () => {
                                                             </td>
                                                         </tr>
                                                     )) :
-                                                        <tr  style={{ color: COLORS.primary }} className="bg-[#F8F8F8] rounded-lg">
-                                                            
+                                                        <tr style={{ color: COLORS.primary }} className="bg-[#F8F8F8] rounded-lg">
+
                                                             <td style={{ ...FONTS.table_data }} className="px-4 py-3 text-center rounded-lg" colSpan={4}>No Slip Founded</td>
-                                                            
+
                                                         </tr>
                                                     }
                                                 </tbody>
